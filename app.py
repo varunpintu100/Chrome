b8af49c4ac3246744f99388c78a269120076d168
varunpintu100 <49563340+varunpintu100@users.noreply.github.com>
2022-05-31 13:09:04 +0530

    Resume link added

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from main import Chrome
 
-
+# this class is to invoke the chrome from the main file
 chrome_1 = Chrome()
+
+#this method is to return the driver from remote 
 driver = chrome_1.driver()
 
-driver.get("https://www.google.com")
+#this is used to navigate to the respected url
+driver.get("https://varun-resume.herokuapp.com")
 
 print(driver.page_source)
