5cff7d24058184844748bb73b79470749d47f3c4
varunpintu100 <49563340+varunpintu100@users.noreply.github.com>
2022-06-02 12:45:19 +0530

    comments are added

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 #this is used the security key
 app.secret_key='Varun'
 
+#this is to use the driver globally
 global driver
 
 #this is used to create the page
@@ -20,26 +21,32 @@ def select_browsers():
 
 @app.route("/browser",methods=["POST"])
 def browser():
+    #this medthod is used to try and find the exceptions if needed.
     try:
         browser_name = request.form.get('browsers')
+        #this is the condition used to check the data whether the name of the browser is correct or not
         if browser_name=='chrome':
+
+            #this temp is used to store the text form the xpath and store it statically
             temp=""
 # this class is to invoke the chrome from the main file
             chrome_1 = Chrome()
 #this method is to return the driver from remote 
             driver = chrome_1.driver()
-
+#this is used to get the data from the form in the html
             url = str(request.form["url_input"])
-
+#this is used to add https: in the starting of the url
             url="https:"+url
+            #this is used to get the type of action that needs to be performed
             action_item = request.form.get('Actions')
-
+            #this is used to get the xpath info from the forms
             xpath = str(request.form["Xpath_info"])
-
+            #this is o get the url in the console
             print(url)
 #this is used to navigate to the respected url
             driver.get(url)
 
+            #this is used to perform the action item and based on the if condition respective action will happen
             if action_item == "click":
 
                 driver.find_element_by_xpath(xpath=xpath).click()
@@ -52,12 +59,15 @@ def browser():
 
                 driver.find_element_by_xpath(xpath=xpath).send_keys("Varun")
 
+            #this is used to store the source of the page as a string
             source = str(driver.page_source)
             driver.quit()
+            #this is used to check weather we are getting any text if no text is found then it returns the webpage source
             if temp=="":
                 return source,200
             return temp
         return browser_name
+#this is used to quit the driver when some exception occurs all of a sudden.
     except:
         driver.quit() 
             
