849694e9884f779caf807cd8c41a7c68135ef2f1
varunpintu100 <49563340+varunpintu100@users.noreply.github.com>
2022-06-06 12:30:08 +0530

    Version 0.3

--- static/script/indexscript.js
+++ static/script/indexscript.js
@@ -12,8 +12,9 @@ function display_c(){
 }// This is used to display the clock under the span
 function display_ct() {
     var x = new Date()
+    var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
     var x1=x.getMonth() + 1+ "/" + x.getDate() + "/" + x.getFullYear(); 
-    x1 = x1 + " - " +  x.getHours( )+ ":" +  x.getMinutes() + ":" +  x.getSeconds();
+    x1 = x1 + " - " +  x.getHours( )+ ":" +  x.getMinutes() + ":" +  x.getSeconds() + ":" +ampm;
     document.getElementById('ct').innerHTML = x1;
     display_c();
      }
