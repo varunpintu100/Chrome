e74bdcdee7ab83205d6002f999e20335c4c798ad
varunpintu100 <49563340+varunpintu100@users.noreply.github.com>
2022-06-06 12:33:40 +0530

    Version 0.3

--- static/script/indexscript.js
+++ static/script/indexscript.js
@@ -14,7 +14,7 @@ function display_ct() {
     var x = new Date()
     var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
     var x1=x.getMonth() + 1+ "/" + x.getDate() + "/" + x.getFullYear(); 
-    x1 = x1 + " - " +  x.getHours( )+ ":" +  x.getMinutes() + ":" +  x.getSeconds() + ":" +ampm;
+    x1 = x1 + " - " +  x.getHours( )+ ":" +  x.getMinutes() + ":" +  x.getSeconds() + "--" +ampm;
     document.getElementById('ct').innerHTML = x1;
     display_c();
      }
