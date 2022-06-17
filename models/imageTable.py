cdd9bc97ecc33ed435f8fa3b929e2af864f27b55
varunpintu100 <49563340+varunpintu100@users.noreply.github.com>
2022-06-17 14:15:30 +0530

    Primary Key

--- models/imageTable.py
+++ models/imageTable.py
@@ -6,7 +6,7 @@ class IMG(db.Model):
 
     #We are creating the coloumns for the table in the database
 
-    id=db.Column(db.Integer,nullable=False)
+    id=db.Column(db.Integer,primary_key=True)
     img=db.Column(db.Text,unique=True,nullable=False)
     mimetype=db.Column(db.Text,nullable=False)
     Xpath=db.Column(db.Text,nullable=False)
