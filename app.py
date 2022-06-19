2288355ccbe63210f922a03826a375d393b4e09d
varunpintu100 <49563340+varunpintu100@users.noreply.github.com>
2022-06-19 21:44:09 +0530

    report added

--- app.py
+++ app.py
@@ -223,8 +223,8 @@ def browser():
             url_list=[]
             for j in test:
                 url_list.append(j.img)
-            url_list=url_list[-i:]
-            return render_template('report.html',i=i,urls=url_list,info_list=lt)
+            url_list=url_list[-(i+1):]
+            return render_template('report.html',i=i+1,urls=url_list,info_list=lt)
         return browser_name
 #this is used to quit the driver when some exception occurs all of a sudden.
     except Exception as e:
