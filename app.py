d8edf5847a1fd572e085f37afa4e22c76ee22160
varunpintu100 <49563340+varunpintu100@users.noreply.github.com>
2022-06-19 10:00:17 +0530

    Removed base64

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Copyright(c) PROJECTCODE. All rights reserved.
 
 '''
 
-import base64
+
 import mimetypes
 import traceback
 import os
@@ -99,7 +99,7 @@ def browser():
 
                         mimetype = mimetypes.guess_type(location)
 
-                        img = IMG(img=base64.b64encode(fp.read().replace("\x00", "\uFFFD")),Xpath=xpath[i],Name=location,mimetype=mimetype[0])
+                        img = IMG(img=fp.read().replace("\x00", "\uFFFD"),Xpath=xpath[i],Name=location,mimetype=mimetype[0])
 
                         img.save_to_db()
 
@@ -115,7 +115,7 @@ def browser():
 
                         fp = open(location,'rb',encoding="utf8",errors='replace')
 
-                        img = IMG(img=base64.b64encode(fp.read().replace("\x00", "\uFFFD")),Xpath=xpath[i],Name=location,mimetype=mimetype[0])
+                        img = IMG(img=fp.read().replace("\x00", "\uFFFD"),Xpath=xpath[i],Name=location,mimetype=mimetype[0])
 
                         img.save_to_db()
 
@@ -134,7 +134,7 @@ def browser():
 
                         mimetype = mimetypes.guess_type(location)
 
-                        img = IMG(img=base64.b64encode(fp.read().replace("\x00", "\uFFFD")),Xpath=xpath[i],Name=location,mimetype=mimetype[0])
+                        img = IMG(img=fp.read().replace("\x00", "\uFFFD"),Xpath=xpath[i],Name=location,mimetype=mimetype[0])
 
                         img.save_to_db()
 
@@ -152,7 +152,7 @@ def browser():
 
                         mimetype = mimetypes.guess_type(location)
 
-                        img = IMG(img=base64.b64encode(fp.read().replace("\x00", "\uFFFD")),Xpath=xpath[i],Name=location,mimetype=mimetype[0])
+                        img = IMG(img=fp.read().replace("\x00", "\uFFFD"),Xpath=xpath[i],Name=location,mimetype=mimetype[0])
 
                         img.save_to_db()
 
@@ -170,7 +170,7 @@ def browser():
 
                         mimetype = mimetypes.guess_type(location)
 
-                        img = IMG(img=base64.b64encode(fp.read().replace("\x00", "\uFFFD")),Xpath=xpath[i],Name=location,mimetype=mimetype[0])
+                        img = IMG(img=fp.read().replace("\x00", "\uFFFD"),Xpath=xpath[i],Name=location,mimetype=mimetype[0])
 
                         img.save_to_db()
 
@@ -188,7 +188,7 @@ def browser():
 
                         mimetype = mimetypes.guess_type(location)
 
-                        img = IMG(img=base64.b64encode(fp.read().replace("\x00", "\uFFFD")),Xpath=xpath[i],Name=location,mimetype=mimetype[0])
+                        img = IMG(img=fp.read().replace("\x00", "\uFFFD"),Xpath=xpath[i],Name=location,mimetype=mimetype[0])
 
                         img.save_to_db()
 
