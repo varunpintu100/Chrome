f826a4c47b5249742569c126c1b592a0333bdc73
varunpintu100 <49563340+varunpintu100@users.noreply.github.com>
2022-06-23 13:12:30 +0530

    Resultion change

--- models/driv.py
+++ models/driv.py
@@ -7,15 +7,14 @@ from selenium.webdriver.common.keys import Keys
 Cloud_upload = Cloud()
 
 class Chrome:
-    capabilities = {
- "resolution": "1024x768"
-}
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("start-maximized")
     chrome_options.add_argument("--no-sandbox")
+    
 
 
     def driver(self):
