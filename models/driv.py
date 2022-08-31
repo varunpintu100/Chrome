1a7036a21d0968e03c74d66c10f1b1ea2b2873cd
varunpintu100 <49563340+varunpintu100@users.noreply.github.com>
2022-08-31 20:41:29 +0530

    Test Data

--- models/driv.py
+++ models/driv.py
@@ -11,7 +11,7 @@ class Chrome:
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("start-maximized")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--remote-debugging-port=9222")
 
